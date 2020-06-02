25e13c9d947dff8c3a057a2bdae674aecd146b31
thesergiuleu <sergiu.leu@smartdata.solutions>
2020-06-02 11:45:25 +0300

    project commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TodoApp
+
+Code-ul aplicatiei pentru insarcinari. care sunt salvate in localstorage in browser si pot fi modificate, putem adauga noi si sa le listam. Aceasta implementare poate fi imbunatatita in viitor cu un backend api care va face actiunile standarte si va salva tot intro baza de date.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../classes/todo';
+
+@Injectable()
+export class TodoService {
+  private nextId: number;
+
+  constructor() {
+    let todos = this.getTodos();
+
+    // if no todos, nextId is 0,
+    // otherwise set to 1 more than last todo id
+    if (todos.length == 0) {
+      this.nextId = 0;
+    } else {
+      let maxId = todos[todos.length-1].id;
+      this.nextId = maxId + 1;
+    }
+  }
+
+  public addTodo(text: string): void {
+    let todo = new Todo(this.nextId, text);
+    let todos = this.getTodos();
+    todos.push(todo);
+
+    // save the todos to local storage
+    this.setLocalStorageTodos(todos);
+    this.nextId++;
+  }
+
+  public getTodos(): Todo[] {
+    let localStorageItem = JSON.parse(localStorage.getItem('todos'));
+    return localStorageItem == null ? [] : localStorageItem.todos;
+  }
+
+  public removeTodo(id: number): void {
+    let todos = this.getTodos();
+    todos = todos.filter((todo)=> todo.id != id);
+    this.setLocalStorageTodos(todos);
+  }
+
+  // private function to help save to local storage
+  private setLocalStorageTodos(todos: Todo[]): void {
+    localStorage.setItem('todos', JSON.stringify({ todos: todos }));
+  }
+}
